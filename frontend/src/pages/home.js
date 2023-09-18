c9aef56f12133cff6e477660553ee5a6bf4106b9
cfyjoanna <joannachanfy@gmail.com>
2023-09-18 21:32:46 +0800

    create subpages

---
+++ frontend/src/pages/home.js
@@ -0,0 +1,35 @@
+import '../App.css';
+import React, { useEffect, useState } from 'react';
+import { Link } from 'react-router-dom';
+
+function Home() {
+
+  const [backendData, setBackendData] = useState('');
+
+  useEffect(() => {
+    fetch("/test").then(
+      response => response.json()
+    ).then(
+      data => {
+        setBackendData(data)
+      }
+    )
+  }, []);
+
+  return (
+    <div className="App">
+      <p>peerprep WOO</p>
+      {(typeof backendData == 'undefined') ? (
+        <p>Loading...</p>
+      ) : (
+        <p>
+          {backendData}<br />
+          <Link to='/questions'>Question Repository</Link> |
+          Another page here
+        </p>
+      )}
+    </div>
+  );
+}
+
+export default Home;
---
+++ frontend/src/pages/questions.js
@@ -0,0 +1,12 @@
+import '../App.css';
+import React from 'react';
+
+function Questions() {
+  return (
+    <div className="App">
+      <p>This is the questions page :)</p>
+    </div>
+  );
+}
+
+export default Questions;
