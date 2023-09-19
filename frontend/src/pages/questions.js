85a7fbee2ff24c0db1b195ea595eadc8ebc53635
cfyjoanna <joannachanfy@gmail.com>
2023-09-20 00:36:54 +0800

    add data persistence using js cookies
    Minimal edits were done since these will be converted to database
    storage, so the setQuestion functions in other files were not renamed.

--- frontend/src/pages/questions.js
+++ frontend/src/pages/questions.js
@@ -1,5 +1,6 @@
 import '../App.css';
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
+import Cookies from 'js-cookie';
 
 import FormDialog from '../components/questions/formDialog.js';
 import QuestionsTable from '../components/questions/questionsTable.js';
@@ -18,18 +19,37 @@ const rows = [
 ];
 
 function Questions() {
-  const [questions, setQuestions] = useState(rows);
+  const [questions, setQuestions] = useState([]);
+
+  // update questions state and store it in a cookie
+  const updateQuestionsAndCookie = (newQuestions) => {
+    setQuestions(newQuestions);
+    Cookies.set('questions', JSON.stringify(newQuestions), { expires: 7 }); // store the data for 7 days
+  };
 
   const handleDelete = (questionId) => {
     const updatedQuestions = questions.filter((question) => question.id !== questionId);
     setQuestions(updatedQuestions);
+    Cookies.set('questions', JSON.stringify(updatedQuestions), { expires: 7 });
   };
+
+  // retrieve data from cookies (if there is) when initialized
+  useEffect(() => {
+    const storedQuestions = Cookies.get('questions');
+    if (storedQuestions) {
+      // there are questions in the cookies
+      setQuestions(JSON.parse(storedQuestions));
+    } else {
+      // cookies is empty
+      setQuestions(rows);
+    }
+  }, []);
 
   return (
     <div className="wrapper">
       <h1>Questions Repository</h1>
       {/* Button to add a new question */}
-      <FormDialog questions={questions} setQuestions={setQuestions} createQuestion={createQuestion} />
+      <FormDialog questions={questions} setQuestions={updateQuestionsAndCookie} createQuestion={createQuestion} />
       {/* Table displaying questions */}
       <QuestionsTable questions={questions} handleDelete={handleDelete} />
     </div>
