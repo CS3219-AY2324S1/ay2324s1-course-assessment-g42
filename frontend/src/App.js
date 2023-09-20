de8744b223eb3e4379bd5f2caa1226746282fedb
cfyjoanna <joannachanfy@gmail.com>
2023-09-20 22:04:38 +0800

    set questions page as SPA

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,15 +2,13 @@ import './App.css';
 import React from 'react';
 import { Route, Routes } from 'react-router-dom';
 
-import Home from './pages/home';
 import Questions from './pages/questions';
 
 function App() {
   return (
     <div className="App">
       <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="questions" element={<Questions />} />
+        <Route path="/" element={<Questions />} />
       </Routes>
     </div>
   );
