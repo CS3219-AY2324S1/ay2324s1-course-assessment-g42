d05c8225c3b2a0d62b5203b2619acac1aceedd6a
cfyjoanna <joannachanfy@gmail.com>
2023-10-24 11:57:49 +0800

    Add gradient box

--- frontend/src/pages/home.js
+++ frontend/src/pages/home.js
@@ -1,11 +1,25 @@
 import '../App.css';
 import React from 'react'
+import { useNavigate } from 'react-router-dom';
+import { Button } from '@mui/material';
+
+import { standardButton } from '../styles/muiButtonStyles';
 
 function Home() {
-    
+  const navigate = useNavigate();
+
+  const handleToRegister = () => {
+    navigate('/signup');
+  }
+
     return (
-        <div className="App">
-            <p>peerprep WOO</p>
+        <div className="grad-box">
+            <p className="home-title-text">A Better Way to Learn</p>
+            <p>PeerPrep offers a collaborative space to enhance skills, improve communication of concepts,
+              and prepare for technical interviews.</p>
+            <div style={{ marginTop: "30px" }}>
+              <Button style={standardButton} onClick={handleToRegister}>Register Now!</Button>
+            </div>
         </div>
     );
 }
