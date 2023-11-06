9506b6d106fd6d34cc92021caa535fc9c086b585
farisa <87534544+farisanadia@users.noreply.github.com>
2023-11-06 17:38:19 +0800

    bugfix logout user overflow

--- frontend/src/components/users/logoutUser.js
+++ frontend/src/components/users/logoutUser.js
@@ -14,8 +14,8 @@ function LogoutUser({ color }) {
     
 
     return(
-        <div style={{ border: '1px solid white', borderRadius: '6px', width: '40%', marginRight: '10px', overflow: 'hidden' }}>
-            <Button variant="text" sx={{ color: color, fontSize: '14px' }} onClick={handleLogout}>Logout</Button>
+        <div style={{ border: '1px solid white', borderRadius: '6px', marginRight: '10px', overflow: 'hidden' }}>
+            <Button variant="text" sx={{ color: color, fontSize: '13px' }} onClick={handleLogout}>Logout</Button>
         </div>
     )
 }
