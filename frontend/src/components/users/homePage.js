90731565161b9e367198ee30d0e27c807e774c6a
phyliciachristel <e0775641@u.nus.edu>
2023-11-07 23:21:05 +0800

    separate home page into components

---
+++ frontend/src/components/users/homePage.js
@@ -0,0 +1,13 @@
+import '../../App.css';
+import React from 'react';
+import QuestionHistory from "./questionHistory";
+
+
+
+function HomePage() {
+  return (
+    <QuestionHistory />
+  );
+}
+
+export default HomePage;
