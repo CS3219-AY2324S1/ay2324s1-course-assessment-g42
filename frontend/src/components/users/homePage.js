38bad67ed3697db81d92c4ecc547a50bd1d9ed51
phyliciachristel <e0775641@u.nus.edu>
2023-11-07 23:35:24 +0800

    add basic skeleton

--- frontend/src/components/users/homePage.js
+++ frontend/src/components/users/homePage.js
@@ -1,12 +1,31 @@
 import '../../App.css';
 import React from 'react';
 import QuestionHistory from "./questionHistory";
+import { Button } from '@mui/material';
+import { Link } from 'react-router-dom';
 
 
 
 function HomePage() {
   return (
-    <QuestionHistory />
+    <>
+      <QuestionHistory />
+      <Button
+          variant="contained"
+          component={Link} to="/match"
+          style={{ backgroundColor: '#F24E1E', color: 'black', padding: '10px', marginLeft: '10px' }}
+      >
+        Start Collaborating!
+      </Button>
+      <Button
+          variant="contained"
+          component={Link} to="/questions/1"
+          style={{ backgroundColor: '#F24E1E', color: 'black', padding: '10px', marginLeft: '10px' }}
+      >
+        View Questions
+      </Button>
+    </>
+    
   );
 }
 
