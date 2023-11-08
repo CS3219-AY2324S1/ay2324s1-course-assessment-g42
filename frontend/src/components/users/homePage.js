9079b9d807e64d7be7067796050ab1e797b24dfd
phyliciachristel <e0775641@u.nus.edu>
2023-11-08 15:52:55 +0800

    finish ui

--- frontend/src/components/users/homePage.js
+++ frontend/src/components/users/homePage.js
@@ -13,36 +13,29 @@ function HomePage() {
     <>
       <div className='rounded-outline-box'>
         <QuestionHistory />
-      </div>
-      
-      <div style={{ marginTop: "10px" }}>
-        <Grid container spacing={0.5} direction="row" justifyContent="center" alignItems="center">
-          <Grid item xs={3}>
-            <div className='center-column-wrapper'>
-              <img src={homeImage1} alt="home_1" style={{ maxWidth: '400px', maxHeight: '300px' }}/>
-              <div className='home-heading-wrapper'>
-                <p className='home-heading'>Work with a friend and take that step towards your dream job</p>
-            
-              </div>
-              <div style={{marginTop: "10px"}}>
-              <Button
+      </div>     
+      <div>
+        <Grid container spacing={3} direction="row" justifyContent="center" alignItems="center">
+          <Grid item xs={4}>
+            <div className='home-heading-wrapper'>
+              <img src={homeImage1} alt="home_1" style={{ maxWidth: '500px', maxHeight: '300px' }}/>
+              <span align='center' className='home-heading'>Work with a friend and take that step towards your dream job</span>
+              <div style={{marginBottom: "10px"}}>
+                <Button
                   variant="contained"
                   component={Link} to="/match"
                   style={{ backgroundColor: '#F24E1E', color: 'black', padding: '10px' }}
                 >
                   Start Collaborating!
                 </Button>
-              </div>
-            </div>
-            
+              </div>                       
+            </div>            
           </Grid>
-          <Grid item xs={3}>
-            <div className='center-column-wrapper'>
+          <Grid item xs={4}>
+            <div className='home-heading-wrapper'>
               <img src={homeImage2} alt="home_2" style={{ maxWidth: '400px', maxHeight: '300px' }}/>
-              <div className='home-heading-wrapper'>
-                <p className='home-heading'>Challenge yourself with one of our challenging questions!</p>
-              </div>
-              <div style={{marginTop: "10px"}}>
+              <span align='center' className='home-heading'>Challenge yourself with one of our challenging questions!</span>                
+              <div style={{marginBottom: "10px"}}>
                 <Button
                   variant="contained"
                   component={Link} to="/questions/1"
@@ -51,14 +44,11 @@ function HomePage() {
                   View Questions
                 </Button>
               </div>
-            </div>
-            
+            </div>            
           </Grid>
         </Grid>      
-      </div>
-      
-    </>
-    
+      </div>      
+    </>    
   );
 }
 
